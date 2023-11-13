ce2bfdcdc8dc1d5f143b40e7481900949d3252b5
danecluster <danecluster@gmail.com>
2023-11-13 06:43:57 -0600

    added role table

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,16 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-  id INT NOT NULL,
-  name VARCHAR(30) NOT NULL,
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,2),
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE CASCADE
 );
